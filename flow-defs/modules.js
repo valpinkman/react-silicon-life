1e13a7b81a840fe06d297b3a6be829494575ef10
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2018-03-12 15:00:54 +0100

    :wrench: fix flow

---
+++ flow-defs/modules.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+declare var module: {
+  hot: {
+    accept(path: string, callback: () => void): void,
+  },
+}
+
+declare var require: any
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import configureStore from './store'
-import App from './components/App'
+import App from './components/app'
 
 const store = configureStore({})
 
