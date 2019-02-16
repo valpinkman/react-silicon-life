c71ef3920a503f39560d6e8249ce13c366328f0a
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2019-02-16 14:00:51 +0100

    v2 with typescript and parcel

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+import Title from './components/Title'
+
+const root = document.getElementById('main')
+
+render(<Title>Hello World</Title>, root)
