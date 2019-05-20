3846844256fbbe5687ae4596d856cf0ae4248c56
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2019-05-20 16:55:41 +0200

    use stylesheet and  fix eslint

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
-import { Text } from 'react-native'
+import { Text, StyleSheet } from 'react-native'
 
 interface TitleProps {
   children: React.ReactNode;
 }
 
-const Title: React.FunctionComponent<TitleProps> = ({ children }: TitleProps) => <Text>{children}</Text>
+const Title: React.FunctionComponent<TitleProps> = ({ children }: TitleProps) => <Text style={styles.text}>{children}</Text>
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 24
+  }
+})
 
 export default Title
